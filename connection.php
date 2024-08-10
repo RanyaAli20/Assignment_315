b3a73ddfffcaee8429c2d894daae780210d7fd2d
RanyaAli20 <ranyaali.2000918@gmail.com>
2024-08-10 18:10:05 +0200

    con

--- connection.php
+++ connection.php
@@ -7,7 +7,6 @@ $dbname = "reservation";
 try {
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "cooooonnnnn";
 	}
 catch(PDOException $e){
     echo "Error: " . $e->getMessage();
