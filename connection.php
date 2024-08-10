ba8cd334a64b31dfbbd71a12433282e9b1aeef51
daniabrebishhh <156687688+daniabrebishhh@users.noreply.github.com>
2024-08-10 23:22:50 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,6 +7,7 @@ $dbname = "reservation";
 try {
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "cooooonnnnn";
 	}
 catch(PDOException $e){
     echo "Error: " . $e->getMessage();
